ffe0f8a26eed28762983552eb168f83d2990870c
Nicholas McDonald <nikmcdonald@outlook.com>
2016-06-11 17:30:59 -0300

    Add comparison and look-up functions

--- lib/rank.rb
+++ lib/rank.rb
@@ -1,8 +1,22 @@
 class Rank
+	include Comparable
+
 	attr_reader :name
 
 	def initialize(name)
 		@name = name
+	end
+
+	def self.get(search_rank)
+		ALL.find { |rank| rank.name == search_rank }
+	end
+
+	def value
+		ALL.find_index self
+	end
+
+	def <=>(other)
+		value <=> other.value
 	end
 
 	ALL = [
--- spec/rank_spec.rb
+++ spec/rank_spec.rb
@@ -1,7 +1,7 @@
 require 'rank'
 
 describe Rank do
-	describe ".name" do
+	describe "#name" do
 		it "returns the Rank name" do
 			expect(Rank.new(:test).name).to eq :test
 		end
@@ -14,6 +14,34 @@ describe Rank do
 
 		it "ends at :ace" do
 			expect(Rank::ALL[Rank::ALL.count - 1].name).to eq :ace
+		end
+	end
+
+	describe ".get" do
+		context "given a rank name" do
+			it "returns the Rank object with that name" do
+				expect(Rank.get :five).to equal Rank::ALL[3]
+			end
+		end
+	end
+
+	describe "#value" do
+		it "returns the Rank's index in ALL" do
+			expect( (Rank.get :ace).value).to eq 12
+		end
+	end
+
+	describe "<=>" do
+		context "given two equal Ranks" do
+			it "finds them to be equal" do
+				expect(Rank.get(:eight)).to eq Rank.get :eight
+			end
+		end
+
+		context "given a larger and smaller Rank" do
+			it "finds the larger to be larger" do
+				expect(Rank.get :seven).to be > (Rank.get :four)
+			end
 		end
 	end
 end
