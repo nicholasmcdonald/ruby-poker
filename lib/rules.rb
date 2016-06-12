3b2361aa9dbdda8f46982f1882432df2f03c9de5
Nicholas McDonald <nikmcdonald@outlook.com>
2016-06-12 15:14:43 -0300

    Add .find_connected_in to Rules; refactoring

--- lib/rules.rb
+++ lib/rules.rb
@@ -1,23 +1,33 @@
 module Rules
 	def Rules.find_multiples_in(desired_matches, pile)
 		pile.each do |card|
-			matches = all_ranked card.rank, pile
+			matches = find_ranked_in card.rank, pile
 			if matches.size >= desired_matches then return matches end
 		end
 
 		return Array.new
 	end
 
-	def Rules.all_ranked(rank, pile)
-		pile.reject{|card| !card.has_rank? rank}
-	end
-
-	def Rules.find_flush_in(pile)
-		Suit::ALL.each do |suit|
-			matches = pile.reject{|card| !card.has_suit? suit}
-			if matches.size >= 5 then return matches end
+	def Rules.find_connected_in(desired_streak, pile)
+		pile.sort.reverse.each do |high_card|
+			connected_cards = Array.new
+			(0...desired_streak).each do |offset|
+				check_rank = high_card.rank.offset_by -offset
+				matches = Rules.find_ranked_in(check_rank, pile)
+				break unless matches.size > 0
+				connected_cards.concat matches
+				return connected_cards if offset == desired_streak - 1
+			end
 		end
 
 		return Array.new
+	end
+
+	def Rules.find_ranked_in(rank, pile)
+		pile.reject{|card| !card.has_rank? rank}
+	end
+
+	def Rules.find_suited_in(suit, pile)
+		pile.reject{|card| !card.has_suit? suit}
 	end
 end
--- spec/rules_spec.rb
+++ spec/rules_spec.rb
@@ -1,12 +1,12 @@
 require 'rules'
 
 describe Rules do
-	describe ".all_ranked" do
+	describe ".find_ranked_in" do
 		context "given an empty pile" do
 			it "finds no matches" do
 				table = Table.new
 				rank = Rank.is(:five)
-				expect(Rules.all_ranked(rank, table.cards_in_play).size)
+				expect(Rules.find_ranked_in(rank, table.cards_in_play).size)
 					.to eq 0
 			end
 		end
@@ -15,7 +15,7 @@ describe Rules do
 			it "returns four cards of that rank" do
 				table = Table.new
 				rank = Rank.is :jack
-				matches = Rules.all_ranked(rank, table.undealt_cards)
+				matches = Rules.find_ranked_in(rank, table.undealt_cards)
 				expect(matches.size).to eq 4
 				expect(matches[0].has_rank? rank).to be true
 			end
@@ -29,7 +29,7 @@ describe Rules do
 				table.add_community_card_at(card, 0)
 				card = Card.new(rank, Suit.is(:diamonds))
 				table.add_hole_card_at(card, 0)
-				matches = Rules.all_ranked(rank, table.cards_in_play)
+				matches = Rules.find_ranked_in(rank, table.cards_in_play)
 				expect(matches.size).to eq 2
 				expect(matches[0].has_rank? rank).to be true
 			end
@@ -74,16 +74,112 @@ describe Rules do
 		end
 	end
 
-	describe ".find_flush_in" do
-		context "given a fresh deck" do
+	describe ".find_suited_in" do
+		context "given a fresh deck and spades argument" do
 			it "finds all of the spades" do
-				flush = Rules.find_flush_in(Table.new.undealt_cards)
+				suit = Suit.is :spades
+				flush = Rules.find_suited_in(suit, Table.new.undealt_cards)
 				expect(flush.size).to eq 13
 				expect(flush[0].has_suit? Suit.is(:spades)).to be true
 			end
 		end
 	end
+
+	describe ".find_connected_in" do
+		context "given a fresh deck and 0 desired streak" do
+			it "returns an empty array" do
+				expect(Rules.find_connected_in(0, Table.new.undealt_cards).size)
+					.to eq 0
+			end
+		end
+
+		context "given a fresh deck and 13 desired streak" do
+			it "finds all the cards" do
+				expect(Rules.find_connected_in(13, Table.new.undealt_cards).size)
+					.to eq 52
+			end
+		end
+
+		context "given a table with five cards in a row, ace high" do
+			it "returns those five cards" do
+				table = Table.new
+				card = Card.new(Rank.is(:ace), Suit.is(:clubs))
+				table.add_community_card_at(card, 4)
+				card = Card.new(Rank.is(:king), Suit.is(:hearts))
+				table.add_community_card_at(card, 1)
+				card = Card.new(Rank.is(:queen), Suit.is(:diamonds))
+				table.add_community_card_at(card, 3)
+				card = Card.new(Rank.is(:jack), Suit.is(:clubs))
+				table.add_community_card_at(card, 0)
+				card = Card.new(Rank.is(:ten), Suit.is(:spades))
+				table.add_hole_card_at(card, 1)
+				straight = Rules.find_connected_in(5, table.cards_in_play)
+				expect(straight.size).to eq 5
+				expect(straight[0].rank).to eq Rank.is(:ace) 
+				expect(straight[4].rank).to eq Rank.is(:ten) 
+			end
+		end
+
+		context "given a table with five cards in a row, including duplicates" do
+			it "returns the five different ranks and each duplicate" do
+				table = Table.new
+				card = Card.new(Rank.is(:seven), Suit.is(:clubs))
+				table.add_community_card_at(card, 4)
+				card = Card.new(Rank.is(:six), Suit.is(:hearts))
+				table.add_community_card_at(card, 1)
+				card = Card.new(Rank.is(:six), Suit.is(:diamonds))
+				table.add_community_card_at(card, 3)
+				card = Card.new(Rank.is(:five), Suit.is(:clubs))
+				table.add_community_card_at(card, 0)
+				card = Card.new(Rank.is(:four), Suit.is(:spades))
+				table.add_hole_card_at(card, 1)
+				card = Card.new(Rank.is(:four), Suit.is(:clubs))
+				table.add_hole_card_at(card, 0)
+				card = Card.new(Rank.is(:three), Suit.is(:diamonds))
+				table.add_community_card_at(card, 2)
+				straight = Rules.find_connected_in(5, table.cards_in_play)
+				expect(straight.size).to eq 7
+				expect(straight[0].rank).to eq Rank.is(:seven) 
+				expect(straight[6].rank).to eq Rank.is(:three) 				
+			end
+		end
+
+		context "given a table with five cards in a row, ace low" do
+			it "returns those five cards" do
+				table = Table.new
+				card = Card.new(Rank.is(:five), Suit.is(:clubs))
+				table.add_community_card_at(card, 4)
+				card = Card.new(Rank.is(:four), Suit.is(:hearts))
+				table.add_community_card_at(card, 1)
+				card = Card.new(Rank.is(:three), Suit.is(:diamonds))
+				table.add_community_card_at(card, 3)
+				card = Card.new(Rank.is(:two), Suit.is(:clubs))
+				table.add_community_card_at(card, 0)
+				card = Card.new(Rank.is(:ace), Suit.is(:spades))
+				table.add_hole_card_at(card, 1)
+				straight = Rules.find_connected_in(5, table.cards_in_play)
+				expect(straight.size).to eq 5
+				expect(straight[0].rank).to eq Rank.is(:five) 
+				expect(straight[4].rank).to eq Rank.is(:ace) 
+			end
+		end
+
+		context "given a table with 4-3-2-A-K" do
+			it "return nothing, as this is not a straight" do
+				table = Table.new
+				card = Card.new(Rank.is(:four), Suit.is(:hearts))
+				table.add_community_card_at(card, 1)
+				card = Card.new(Rank.is(:three), Suit.is(:diamonds))
+				table.add_community_card_at(card, 3)
+				card = Card.new(Rank.is(:two), Suit.is(:clubs))
+				table.add_community_card_at(card, 0)
+				card = Card.new(Rank.is(:ace), Suit.is(:spades))
+				table.add_hole_card_at(card, 1)
+				card = Card.new(Rank.is(:king), Suit.is(:clubs))
+				table.add_community_card_at(card, 4)
+				straight = Rules.find_connected_in(5, table.cards_in_play)
+				expect(straight.size).to eq 0
+			end
+		end
+	end
 end
-
-
-
