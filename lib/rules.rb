c34de35a88a372fd10eb62573419bed74ef8172e
Nicholas McDonald <nikmcdonald@outlook.com>
2016-06-12 22:22:18 -0300

    Refactor rules

--- lib/rules.rb
+++ lib/rules.rb
@@ -8,20 +8,38 @@ module Rules
 		return Array.new
 	end
 
-	def self.find_connected_in(length, pile)
-		pile.sort.reverse.each do |high_card|
-			connected_cards = Array.new
-			(0...length).each do |offset|
-				check_rank = high_card.rank.offset_by -offset
-				matches = find_ranked_in(check_rank, pile)
-				break unless matches.size > 0
-				connected_cards.concat matches
-				return connected_cards if offset == length - 1
-			end
-		end
-
-		return Array.new
+	def self.find_candidate_straights(length, pile)
+		package_duplicates(pile).each_cons(length)
+			.select(&method(:connected?))
 	end
+
+	def self.package_duplicates(pile)
+		pile.collect(&:rank)
+			.sort.reverse.uniq
+			.collect{|rank| find_ranked_in(rank, pile)}
+	end
+
+	def self.connected?(candidate)
+		candidate.drop(1).each_with_index.all? { |slot, i|
+			slot[0].has_rank? candidate[i][0].rank.offset_by(-1)
+		}
+	end
+
+	# Obsolete!
+	# def self.find_connected_in(length, pile)
+	# 	pile.sort.reverse.each do |high_card|
+	# 		connected_cards = Array.new
+	# 		(0...length).each do |offset|
+	# 			check_rank = high_card.rank.offset_by -offset
+	# 			matches = find_ranked_in(check_rank, pile)
+	# 			break unless matches.size > 0
+	# 			connected_cards.concat matches
+	# 			return connected_cards if offset == length - 1
+	# 		end
+	# 	end
+
+	# 	return Array.new
+	# end
 
 	def self.find_ranked_in(rank, pile)
 		pile.select{|card| card.has_rank? rank}
--- spec/rules_spec.rb
+++ spec/rules_spec.rb
@@ -85,100 +85,157 @@ describe Rules do
 		end
 	end
 
-	describe ".find_connected_in" do
-		context "given a fresh deck and 0 desired streak" do
-			it "returns an empty array" do
-				expect(Rules.find_connected_in(0, Table.new.undealt_cards).size)
-					.to eq 0
-			end
-		end
+	# describe ".find_connected_in" do
+	# 	context "given a fresh deck and 0 desired streak" do
+	# 		it "returns an empty array" do
+	# 			expect(Rules.find_connected_in(0, Table.new.undealt_cards).size)
+	# 				.to eq 0
+	# 		end
+	# 	end
 
-		context "given a fresh deck and 13 desired streak" do
-			it "finds all the cards" do
-				expect(Rules.find_connected_in(13, Table.new.undealt_cards).size)
-					.to eq 52
-			end
-		end
+	# 	context "given a fresh deck and 13 desired streak" do
+	# 		it "finds all the cards" do
+	# 			expect(Rules.find_connected_in(13, Table.new.undealt_cards).size)
+	# 				.to eq 52
+	# 		end
+	# 	end
 
-		context "given a table with five cards in a row, ace high" do
-			it "returns those five cards" do
+	# 	context "given a table with five cards in a row, ace high" do
+	# 		it "returns those five cards" do
+	# 			table = Table.new
+	# 			card = Card.new(Rank.is(:ace), Suit.is(:clubs))
+	# 			table.add_community_card_at(card, 4)
+	# 			card = Card.new(Rank.is(:king), Suit.is(:hearts))
+	# 			table.add_community_card_at(card, 1)
+	# 			card = Card.new(Rank.is(:queen), Suit.is(:diamonds))
+	# 			table.add_community_card_at(card, 3)
+	# 			card = Card.new(Rank.is(:jack), Suit.is(:clubs))
+	# 			table.add_community_card_at(card, 0)
+	# 			card = Card.new(Rank.is(:ten), Suit.is(:spades))
+	# 			table.add_hole_card_at(card, 1)
+	# 			straight = Rules.find_connected_in(5, table.cards_in_play)
+	# 			expect(straight.size).to eq 5
+	# 			expect(straight[0].rank).to eq Rank.is(:ace) 
+	# 			expect(straight[4].rank).to eq Rank.is(:ten) 
+	# 		end
+	# 	end
+
+	# 	context "given a table with five cards in a row, including duplicates" do
+	# 		it "returns the five different ranks and each duplicate" do
+	# 			table = Table.new
+	# 			card = Card.new(Rank.is(:seven), Suit.is(:clubs))
+	# 			table.add_community_card_at(card, 4)
+	# 			card = Card.new(Rank.is(:six), Suit.is(:hearts))
+	# 			table.add_community_card_at(card, 1)
+	# 			card = Card.new(Rank.is(:six), Suit.is(:diamonds))
+	# 			table.add_community_card_at(card, 3)
+	# 			card = Card.new(Rank.is(:five), Suit.is(:clubs))
+	# 			table.add_community_card_at(card, 0)
+	# 			card = Card.new(Rank.is(:four), Suit.is(:spades))
+	# 			table.add_hole_card_at(card, 1)
+	# 			card = Card.new(Rank.is(:four), Suit.is(:clubs))
+	# 			table.add_hole_card_at(card, 0)
+	# 			card = Card.new(Rank.is(:three), Suit.is(:diamonds))
+	# 			table.add_community_card_at(card, 2)
+	# 			straight = Rules.find_connected_in(5, table.cards_in_play)
+	# 			expect(straight.size).to eq 7
+	# 			expect(straight[0].rank).to eq Rank.is(:seven) 
+	# 			expect(straight[6].rank).to eq Rank.is(:three) 				
+	# 		end
+	# 	end
+
+	# 	context "given a table with five cards in a row, ace low" do
+	# 		it "returns those five cards" do
+	# 			table = Table.new
+	# 			card = Card.new(Rank.is(:five), Suit.is(:clubs))
+	# 			table.add_community_card_at(card, 4)
+	# 			card = Card.new(Rank.is(:four), Suit.is(:hearts))
+	# 			table.add_community_card_at(card, 1)
+	# 			card = Card.new(Rank.is(:three), Suit.is(:diamonds))
+	# 			table.add_community_card_at(card, 3)
+	# 			card = Card.new(Rank.is(:two), Suit.is(:clubs))
+	# 			table.add_community_card_at(card, 0)
+	# 			card = Card.new(Rank.is(:ace), Suit.is(:spades))
+	# 			table.add_hole_card_at(card, 1)
+	# 			straight = Rules.find_connected_in(5, table.cards_in_play)
+	# 			expect(straight.size).to eq 5
+	# 			expect(straight[0].rank).to eq Rank.is(:five) 
+	# 			expect(straight[4].rank).to eq Rank.is(:ace) 
+	# 		end
+	# 	end
+
+	# 	context "given a table with 4-3-2-A-K" do
+	# 		it "return nothing, as this is not a straight" do
+	# 			table = Table.new
+	# 			card = Card.new(Rank.is(:four), Suit.is(:hearts))
+	# 			table.add_community_card_at(card, 1)
+	# 			card = Card.new(Rank.is(:three), Suit.is(:diamonds))
+	# 			table.add_community_card_at(card, 3)
+	# 			card = Card.new(Rank.is(:two), Suit.is(:clubs))
+	# 			table.add_community_card_at(card, 0)
+	# 			card = Card.new(Rank.is(:ace), Suit.is(:spades))
+	# 			table.add_hole_card_at(card, 1)
+	# 			card = Card.new(Rank.is(:king), Suit.is(:clubs))
+	# 			table.add_community_card_at(card, 4)
+	# 			straight = Rules.find_connected_in(5, table.cards_in_play)
+	# 			expect(straight.size).to eq 0
+	# 		end
+	# 	end
+	# end
+
+	describe ".package_duplicates" do
+		context "given seven cards 2-5-7-5-J-A-A" do
+			it "sorts them into arrays with pairs appearing together" do
 				table = Table.new
+				card = Card.new(Rank.is(:two), Suit.is(:hearts))
+				table.add_community_card_at(card, 0)
+				card = Card.new(Rank.is(:five), Suit.is(:diamonds))
+				table.add_community_card_at(card, 1)
+				card = Card.new(Rank.is(:seven), Suit.is(:clubs))
+				table.add_community_card_at(card, 2)
+				card = Card.new(Rank.is(:five), Suit.is(:spades))
+				table.add_hole_card_at(card, 0)
+				card = Card.new(Rank.is(:jack), Suit.is(:clubs))
+				table.add_community_card_at(card, 3)
+				card = Card.new(Rank.is(:ace), Suit.is(:spades))
+				table.add_hole_card_at(card, 1)
 				card = Card.new(Rank.is(:ace), Suit.is(:clubs))
 				table.add_community_card_at(card, 4)
+				result = Rules.package_duplicates(table.cards_in_play)
+				expect(result.size).to eq 5
+				expect(result[0].size).to eq 2
+				expect(result[0][1].rank).to eq Rank.is(:ace)
+				expect(result[3].size).to eq 2
+				expect(result[3][1].rank).to eq Rank.is(:five)
+				expect(result[1].size).to eq 1
+			end
+		end
+	end
+
+	describe ".find_candidate_straights" do
+		context "given seven cards packaged by duplicates" do
+			it "collects all possible straights" do
+				table = Table.new
 				card = Card.new(Rank.is(:king), Suit.is(:hearts))
-				table.add_community_card_at(card, 1)
+				table.add_community_card_at(card, 0)
 				card = Card.new(Rank.is(:queen), Suit.is(:diamonds))
-				table.add_community_card_at(card, 3)
-				card = Card.new(Rank.is(:jack), Suit.is(:clubs))
-				table.add_community_card_at(card, 0)
-				card = Card.new(Rank.is(:ten), Suit.is(:spades))
-				table.add_hole_card_at(card, 1)
-				straight = Rules.find_connected_in(5, table.cards_in_play)
-				expect(straight.size).to eq 5
-				expect(straight[0].rank).to eq Rank.is(:ace) 
-				expect(straight[4].rank).to eq Rank.is(:ten) 
-			end
-		end
-
-		context "given a table with five cards in a row, including duplicates" do
-			it "returns the five different ranks and each duplicate" do
-				table = Table.new
-				card = Card.new(Rank.is(:seven), Suit.is(:clubs))
-				table.add_community_card_at(card, 4)
-				card = Card.new(Rank.is(:six), Suit.is(:hearts))
 				table.add_community_card_at(card, 1)
-				card = Card.new(Rank.is(:six), Suit.is(:diamonds))
-				table.add_community_card_at(card, 3)
-				card = Card.new(Rank.is(:five), Suit.is(:clubs))
-				table.add_community_card_at(card, 0)
-				card = Card.new(Rank.is(:four), Suit.is(:spades))
-				table.add_hole_card_at(card, 1)
-				card = Card.new(Rank.is(:four), Suit.is(:clubs))
-				table.add_hole_card_at(card, 0)
-				card = Card.new(Rank.is(:three), Suit.is(:diamonds))
+				card = Card.new(Rank.is(:nine), Suit.is(:clubs))
 				table.add_community_card_at(card, 2)
-				straight = Rules.find_connected_in(5, table.cards_in_play)
-				expect(straight.size).to eq 7
-				expect(straight[0].rank).to eq Rank.is(:seven) 
-				expect(straight[6].rank).to eq Rank.is(:three) 				
-			end
-		end
-
-		context "given a table with five cards in a row, ace low" do
-			it "returns those five cards" do
-				table = Table.new
-				card = Card.new(Rank.is(:five), Suit.is(:clubs))
-				table.add_community_card_at(card, 4)
-				card = Card.new(Rank.is(:four), Suit.is(:hearts))
-				table.add_community_card_at(card, 1)
-				card = Card.new(Rank.is(:three), Suit.is(:diamonds))
+				card = Card.new(Rank.is(:ten), Suit.is(:spades))
+				table.add_hole_card_at(card, 0)
+				card = Card.new(Rank.is(:jack), Suit.is(:clubs))
 				table.add_community_card_at(card, 3)
-				card = Card.new(Rank.is(:two), Suit.is(:clubs))
-				table.add_community_card_at(card, 0)
 				card = Card.new(Rank.is(:ace), Suit.is(:spades))
 				table.add_hole_card_at(card, 1)
-				straight = Rules.find_connected_in(5, table.cards_in_play)
-				expect(straight.size).to eq 5
-				expect(straight[0].rank).to eq Rank.is(:five) 
-				expect(straight[4].rank).to eq Rank.is(:ace) 
-			end
-		end
-
-		context "given a table with 4-3-2-A-K" do
-			it "return nothing, as this is not a straight" do
-				table = Table.new
-				card = Card.new(Rank.is(:four), Suit.is(:hearts))
-				table.add_community_card_at(card, 1)
-				card = Card.new(Rank.is(:three), Suit.is(:diamonds))
-				table.add_community_card_at(card, 3)
-				card = Card.new(Rank.is(:two), Suit.is(:clubs))
-				table.add_community_card_at(card, 0)
-				card = Card.new(Rank.is(:ace), Suit.is(:spades))
-				table.add_hole_card_at(card, 1)
-				card = Card.new(Rank.is(:king), Suit.is(:clubs))
+				card = Card.new(Rank.is(:ace), Suit.is(:clubs))
 				table.add_community_card_at(card, 4)
-				straight = Rules.find_connected_in(5, table.cards_in_play)
-				expect(straight.size).to eq 0
+				packages = Rules.package_duplicates(table.cards_in_play)
+				results = Rules.find_candidate_straights(5, packages)
+				expect(results.size).to eq 2
+				expect(results[0].size).to eq 5
+				expect(results[0][0][0].rank).to eq Rank.is(:ace)
+				expect(results[1][0][0].rank).to eq Rank.is(:king)
 			end
 		end
 	end
